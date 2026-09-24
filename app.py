26522e89d41fb552b04ab69ad7ea1ba8eaa7620d
juliuskamau <kamaujulius077@gmail.com>
2026-09-24 18:18:46 +0300

    final working app.py

--- app.py
+++ app.py
@@ -1,46 +1,40 @@
-import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO
-from werkzeug.utils import secure_filename
-from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'softech_secret_ke'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///softech.db'
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///houses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs('static/css', exist_ok=True)
-os.makedirs('static/js', exist_ok=True)
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 class House(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150))
+    title = db.Column(db.String(100))
     location = db.Column(db.String(100))
     price = db.Column(db.Integer)
-    beds = db.Column(db.String(20))
+    beds = db.Column(db.String(10))
     phone = db.Column(db.String(20))
     description = db.Column(db.Text)
-    image = db.Column(db.String(200))
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    image = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-try:
-    with app.app_context():
+with app.app_context():
+    try:
         db.create_all()
-except:
-    pass
+    except:
+        pass
 
-
+@app.route('/')
 def home():
     location = request.args.get('location','')
     max_price = request.args.get('max_price','')
     beds = request.args.get('beds','')
     query = House.query
     if location:
-        query = query.filter(House.location.ilike(f'%{location}%'))
+        query = query.filter(House.location.ilike(f"%{location}%"))
     if max_price and max_price.isdigit():
         query = query.filter(House.price <= int(max_price))
     if beds:
@@ -51,28 +45,23 @@ def home():
 @app.route('/post', methods=['GET','POST'])
 def post_house():
     if request.method == 'POST':
-        file = request.files.get('photo')
+        file = request.files.get('image')
         filename = ''
         if file and file.filename:
-            filename = datetime.now().strftime("%H%M%S_") + secure_filename(file.filename)
+            filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
         house = House(
-            title=request.form['title'],
-            location=request.form['location'],
-            price=int(request.form['price']),
-            beds=request.form['beds'],
-            phone=request.form['phone'],
-            description=request.form['description'],
+            title=request.form.get('title'),
+            location=request.form.get('location'),
+            price=int(request.form.get('price',0) or 0),
+            beds=request.form.get('beds'),
+            phone=request.form.get('phone'),
+            description=request.form.get('description'),
             image=filename
         )
         db.session.add(house)
         db.session.commit()
-        socketio.emit('new_house', {
-            'title': house.title, 'location': house.location,
-            'price': house.price, 'beds': house.beds,
-            'phone': house.phone, 'description': house.description,
-            'image': filename
-        })
         return redirect('/')
     return render_template('post.html')
 
@@ -82,4 +71,3 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
